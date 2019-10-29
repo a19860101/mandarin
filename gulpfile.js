b47dbff8ea28e0e2cca75a8bfc3d9ae569f369c6
Zac <a19860101@hotmail.com>
2019-10-29 15:18:32 +0800

    use babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src, dest, series, watch} = require('gulp');
 const gulpSass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
+const gulpBabel = require('gulp-babel');
 
 function css(){
     return src('src/scss/*.scss')
@@ -8,5 +9,10 @@ function css(){
     .pipe(minifyCSS())
     .pipe(dest('dist/css'))
 }
+function babel(){
+    return src('src/js/script.js')
+    .pipe(gulpBabel())
+    .pipe(dest('dist/js'))
+}
 
-exports.default = series(css);
+exports.default = series(css,babel);
