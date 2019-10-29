59cd2922678ce368bea916e297651ffb853b8c67
Zac <a19860101@hotmail.com>
2019-10-29 15:29:47 +0800

    use gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ const {src, dest, series, watch} = require('gulp');
 const gulpSass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
 const gulpBabel = require('gulp-babel');
+const autoprefix = require('gulp-autoprefixer');
 
 function css(){
     return src('src/scss/*.scss')
     .pipe(gulpSass())
+    .pipe(autoprefix())
     .pipe(minifyCSS())
     .pipe(dest('dist/css'))
 }
